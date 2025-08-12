5ab7b459342699c7cb0189ae90ae7c01614f31bb
Eakapob <156561431+Eakapob@users.noreply.github.com>
2025-08-12 21:21:57 +0700

    12/8/2025

--- src/components/form/WalletForm.tsx
+++ src/components/form/WalletForm.tsx
@@ -1,74 +1,96 @@
-'use client';
+"use client";
 
 import { Wallet } from "@prisma/client";
 import { useSession } from "next-auth/react";
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
 const WalletForm = () => {
-    const [namewallet, setNamewallet] = useState('')
-    const [totalbalance, setTotalbalance] = useState<number | ''>('')
+  const [namewallet, setNamewallet] = useState("");
+  const [totalbalance, setTotalbalance] = useState<number | "">("");
 
-    const { data: session } = useSession();
-    const userid = session?.user?.userid;
-    // console.log("userid",userid)
-    const [wallets, setWallets] = useState<Wallet[]>([])
+  const { data: session } = useSession();
+  const userid = session?.user?.userid;
+  // console.log("userid",userid)
+  const [wallets, setWallets] = useState<Wallet[]>([]);
+  const [edit, setEdit] = useState(false);
+  const [adding, setAdding] = useState(false);
 
-    useEffect(() => {
-        fetchWalletData();
-    }, [])
+  useEffect(() => {
+    fetchWalletData();
+  }, []);
 
-    const onSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-        if (!userid) {
-            alert("คุณต้องเข้าสู่ระบบก่อน");
-            return;
-        }
-
-        const res = await fetch('/api/wallet', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ userid, namewallet, totalbalance }),
-        })
-
-        const data = await res.json()
-        if (res.ok) {
-            alert(data.message || 'Submit success!');
-            setNamewallet('');
-            setTotalbalance('');
-            fetchWalletData();
-        } else {
-            alert(data.error || 'Something went wrong!!!')
-        }
+    if (!userid) {
+      alert("คุณต้องเข้าสู่ระบบก่อน");
+      return;
     }
 
-    const fetchWalletData = async () => {
-        const res = await fetch('/api/wallet')
-        const data = await res.json()
-        setWallets(data)
+    const res = await fetch("/api/wallet", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userid, namewallet, totalbalance }),
+    });
+
+    const data = await res.json();
+    if (res.ok) {
+      alert(data.message || "Submit success!");
+      setNamewallet("");
+      setTotalbalance("");
+      fetchWalletData();
+    } else {
+      alert(data.error || "Something went wrong!!!");
     }
+  };
 
-    return (
-        <div>
-            <ul className="w-2.5/12 h-2/12 rounded-lg shadow-md">
-                {wallets.map((wallet) => (
-                    <li key={wallet.walletid}>{wallet.namewallet} - {wallet.totalbalance}</li>
-                ))}
-            </ul>
-            <div className="border-1 border-black shadow-md">
-                <form onSubmit={onSubmit}>
-                    <label>Name wallet</label><br />
-                    <input type="text" value={namewallet} onChange={(e) => setNamewallet(e.target.value)} className="border border-black" /><br />
-                    <label>Amount</label><br />
-                    <input type="number" value={totalbalance} onChange={(e) => setTotalbalance(Number(e.target.value))} className="border border-black" /><br />
-                    <input type="submit" />
-                </form>
-            </div>
-        </div>
-    )
+  const fetchWalletData = async () => {
+    const res = await fetch("/api/wallet");
+    const data = await res.json();
+    setWallets(data);
+  };
 
-}
+  return (
+    <div>
+      <ul className="w-2.5/12 h-2/12 rounded-lg shadow-md">
+        {wallets.map((wallet) => (
+          <li key={wallet.walletid}>
+            {wallet.namewallet} - {wallet.totalbalance}
+          </li>
+        ))}
+      </ul>
+      {edit ? <></> : <button onClick={() => setEdit(true)}>Edit</button>}
+      <div className="border-1 border-black shadow-md">
+        {adding ? (
+          <form onSubmit={onSubmit}>
+            <label>Name wallet</label>
+            <br />
+            <input
+              type="text"
+              value={namewallet}
+              onChange={(e) => setNamewallet(e.target.value)}
+              className="border border-black"
+            />
+            <br />
+            <label>Amount</label>
+            <br />
+            <input
+              type="number"
+              value={totalbalance}
+              onChange={(e) => setTotalbalance(Number(e.target.value))}
+              className="border border-black"
+            />
+            <br />
+            <input type="submit" />
+          </form>
+        ) : (
+          <button onClick={() => setAdding(true)}>Add</button>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default WalletForm;
